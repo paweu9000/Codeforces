24ad61d56eae4090aa73165970f7738a05091668
paweu9000 <wolfenorrin5@gmail.com>
2024-03-05 18:16:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# codeforces
+
+## Intro
+
+>	Automated by [cp-tool](https://github.com/jspw/cp-tool)
+
+This is a repository to keep track of my problem solving practice.
+For now, It contains all the problems I have solved at 
+- **[Codeforces](https://codeforces.com/)** 
+
---
+++ WayTooLongWords.hs
@@ -0,0 +1,18 @@
+module WayTooLongWords where
+
+main :: IO ()
+main = do
+    input <- readLn :: IO Int
+    words <- getWords input []
+    mapM_ putStrLn words
+
+getWords :: Int -> [String] -> IO [String]
+getWords 0 x = return x 
+getWords amount words = do
+    input <- getLine :: IO String
+    getWords (amount-1) (words ++ [transmute input])
+
+transmute :: String -> String
+transmute str
+    | length str > 10 = head str : (show ((length str) - 2)) ++ [last str]
+    | otherwise = str
