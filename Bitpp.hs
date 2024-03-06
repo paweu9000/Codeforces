8ea15bfecc160f48dbc4b1130a49d0829a669364
paweu9000 <wolfenorrin5@gmail.com>
2024-03-06 16:13:02 +0100

    Bitpp solution

---
+++ Bitpp.hs
@@ -0,0 +1,19 @@
+module Main where
+
+main :: IO ()
+main = do
+    amount <- readLn :: IO Int
+    input <- saveInput amount []
+    print $ foldr calculate 0 input
+
+saveInput :: Int -> [String] -> IO [String]
+saveInput 0 x = return x
+saveInput a x = do
+    input <- getLine :: IO String
+    saveInput (a-1) (input:x)
+
+calculate :: String -> Int -> Int
+calculate "X++" x = x + 1
+calculate "++X" x = x + 1
+calculate "X--" x = x - 1
+calculate "--X" x = x - 1
